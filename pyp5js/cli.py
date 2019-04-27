dbc8254a1cafb59079da15e7f01cdf5169dec126
Bernardo Fontes <bernardoxhc@gmail.com>
2019-04-27 16:09:39 -0300

    Fixes sketch js file

--- pyp5js/cli.py
+++ pyp5js/cli.py
@@ -8,11 +8,12 @@ from datetime import date
 from cprint import cprint
 from shutil import copyfile
 from unipath import Path
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader
 
 
 PYP5_DIR = Path(__file__).parent
 TEMPLATES_DIR = PYP5_DIR.child('templates')
+templates = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
 
 @click.group()
 def cli():
@@ -28,6 +29,12 @@ def cli():
 def configure_new_sketch(sketch_name, sketch_dir):
     """
     Create dir and configure boilerplate
+
+    Params:
+    - sketch_name: name of the sketch (will create a {sketch_name}.py)
+
+    Opitionals
+    - sketch_dir: directory to save the sketch (defaults to ./{sketch_name})
     """
     SKETCH_DIR = Path(sketch_dir or f'./{sketch_name}')
     if SKETCH_DIR.exists():
@@ -37,7 +44,6 @@ def configure_new_sketch(sketch_name, sketch_dir):
     static_dir = SKETCH_DIR.child('static')
     templates_files = [
         (TEMPLATES_DIR.child('base_sketch.py'), SKETCH_DIR.child(f'{sketch_name}.py')),
-        (TEMPLATES_DIR.child('index.html'), SKETCH_DIR.child(f'index.html')),
         (PYP5_DIR.child('static', 'p5.js'), static_dir.child('p5.js'))
     ]
 
@@ -45,6 +51,16 @@ def configure_new_sketch(sketch_name, sketch_dir):
     os.mkdir(static_dir)
     for src, dest in templates_files:
         copyfile(src, dest)
+
+    index_template = templates.get_template('index.html')
+    context = {
+        "p5_js_url": "static/p5.js",
+        "sketch_js_url": f"__target__/{sketch_name}.js",
+    }
+    index_contet = index_template.render(context)
+
+    with open(SKETCH_DIR.child("index.html"), "w") as fd:
+        fd.write(index_contet)
 
 
 @cli.command("transcrypt")
@@ -78,3 +94,7 @@ def transcrypt_sketch(sketch_name, sketch_dir, pyp5js):
 
     transcrypt = subprocess.Popen(shlex.split(command))
     transcrypt.wait()
+
+
+if __name__ == "__main__":
+    cli()
