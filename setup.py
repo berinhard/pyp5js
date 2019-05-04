d12e793c023906b99269ef6e60ab4bf4bb29e785
Bernardo Fontes <bernardoxhc@gmail.com>
2019-05-04 12:29:19 -0300

    Fix readme example

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md") as fd:
 
 setup(
     name="pyp5js",
-    version="0.0.1",
+    version="0.0.2",
     description='Simple tool to allow to transcrypt Python code that uses P5.js',
     long_description=long_description,
     long_description_content_type='text/markdown',
