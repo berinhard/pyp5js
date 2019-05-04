58508a40225bebfc5ad52f988a92254ece657fa4
Bernardo Fontes <bernardoxhc@gmail.com>
2019-05-04 12:16:22 -0300

    Improve license

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
+# Copyright 2018-2019 Bernardo Fontes <https://github.com/berinhard/pyp5js/>
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as fd:
@@ -15,7 +30,7 @@ setup(
     author='Bernardo Fontes',
     maintainer='Bernardo Fontes',
     maintainer_email='bernardoxhc@gmail.com',
-    license='GPL 3',
+    license='GNU Lesser General Public License version 3',
     packages=find_packages(),
     package_data={
         'pyp5js': [
