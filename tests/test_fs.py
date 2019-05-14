3afb47095b98edaa95b8aa50e87cd6e0a2a97f88
Bernardo Fontes <bernardoxhc@gmail.com>
2019-05-14 09:06:16 -0300

    Add test method to ensure lib files

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -19,3 +19,24 @@ def test_dir_properties(lib_files):
     assert lib_files.static_dir == pyp5_dir.child('static')
     assert lib_files.static_dir.exists()
 
+
+def test_files_properties(lib_files):
+    assert pyp5_dir.exists()
+
+    assert lib_files.pytop5js == pyp5_dir.child('pytop5js.py')
+    assert lib_files.pytop5js.exists()
+
+    assert lib_files.base_sketch == pyp5_dir.child('templates', 'base_sketch.py')
+    assert lib_files.base_sketch.exists()
+
+    assert lib_files.pytop5js_template == pyp5_dir.child('templates', 'pytop5js.py.template')
+    assert lib_files.pytop5js_template.exists()
+
+    assert lib_files.index_html == pyp5_dir.child('templates', 'index.html')
+    assert lib_files.index_html.exists()
+
+    assert lib_files.p5js == pyp5_dir.child('static', 'p5.js')
+    assert lib_files.p5js.exists()
+
+    assert lib_files.p5_yml == pyp5_dir.child('assets', 'p5_reference.yml')
+    assert lib_files.p5_yml.exists()
