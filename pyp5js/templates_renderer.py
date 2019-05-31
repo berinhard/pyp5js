de99a9d845e46bf9ebfe7f863601f0a667643fd2
Bernardo Fontes <bernardoxhc@gmail.com>
2019-05-31 18:46:12 -0300

    Render function to generate the base template for target_sketch.py

--- pyp5js/templates_renderer.py
+++ pyp5js/templates_renderer.py
@@ -15,3 +15,15 @@ def get_pytop5js_content(variables_names, methods_names, event_function_names):
         'event_function_names': event_function_names,
     }
     return pyp5_template.render(context)
+
+
+def get_target_sketch_template_content(event_function_names):
+    content = "import {{ sketch_name }} as source_sketch\nfrom pytop5js import *\n\n"
+    content += "event_functions = {\n"
+
+    for event in event_function_names:
+        content += f'    "{event}": source_sketch.{event},\n'
+
+    content += '}\n\nstart_p5(sketch_source.setup, sketch_source.draw, event_functions)'
+
+    return content
