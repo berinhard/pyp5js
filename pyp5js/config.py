567529a8d90832927197991163b87e537145e31e
Bernardo Fontes <bernardoxhc@gmail.com>
2019-10-06 15:55:35 -0300

    Ensure the sketchbook dir always exists

--- pyp5js/config.py
+++ pyp5js/config.py
@@ -2,3 +2,6 @@ from decouple import config
 from pathlib import Path
 
 SKETCHBOOK_DIR = config("SKETCHBOOK_DIR", cast=Path, default=Path.home().joinpath('sketchbook-pyp5js'))
+
+if not SKETCHBOOK_DIR.exists():
+    SKETCHBOOK_DIR.mkdir()
