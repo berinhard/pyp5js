8ce707295a339838364db1f0786522c785eaf773
Bernardo Fontes <bernardoxhc@gmail.com>
2019-10-06 11:51:12 -0300

    Add route to serve the sketch's index.html

--- pyp5js/http/web_app.py
+++ pyp5js/http/web_app.py
@@ -15,6 +15,26 @@ def sketches_list_view():
         name = sketch_dir.name
         sketch_files = SketchFiles(name)
         if sketch_files.has_all_files:
-            sketches.append(name)
+            sketches.append({
+                'name': name,
+                'url': f'/sketch/{name}'
+            })
 
     return render_template('index.html', sketches=sketches)
+
+
+@app.route('/sketch/<string:sketch_name>/')
+def render_sketch(sketch_name):
+    sketch_files = SketchFiles(sketch_name)
+
+    msg_404 = ''
+    if not sketch_files.sketch_dir.exists():
+        msg_404 = f"There's no sketch in {sketch_files.sketch_dir.resolve()}"
+    elif not sketch_files.has_all_files:
+        msg_404 = f"The sketch {sketch_name} has missing files."
+
+    if msg_404:
+        return msg_404, 404
+
+    with sketch_files.index_html.open() as fd:
+        return fd.read()
