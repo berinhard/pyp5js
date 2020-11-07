fd5016331351ef8a75e29422e595df66d7c5506c
Bernardo Fontes <bernardoxhc@gmail.com>
2020-11-07 14:25:57 -0300

    Unit test refactoring
    Signed-off-by: Bernardo Fontes <bernardoxhc@gmail.com>

--- pyp5js/tests/test_fs.py
+++ pyp5js/tests/test_fs.py
@@ -58,18 +58,21 @@ class SketchFilesTests(TestCase):
         if self.base_dir.exists():
             shutil.rmtree(self.base_dir)
 
+    def get_expected_path(self, *args):
+        return self.base_dir.joinpath(self.sketch_name, *args)
+
     def test_sketch_dirs(self):
-        assert self.base_dir.joinpath(self.sketch_name) == self.files.sketch_dir
-        assert self.base_dir.joinpath(self.sketch_name, 'static') == self.files.static_dir
-        assert self.base_dir.joinpath(self.sketch_name, 'target') == self.files.target_dir
+        assert self.get_expected_path() == self.files.sketch_dir
+        assert self.get_expected_path('static') == self.files.static_dir
+        assert self.get_expected_path('target') == self.files.target_dir
         assert self.files.TARGET_NAME == 'target'
 
     def test_sketch_files(self):
         self.files.check_sketch_dir = False
-        assert self.base_dir.joinpath(self.sketch_name, 'index.html') == self.files.index_html
-        assert self.base_dir.joinpath(self.sketch_name, 'static', 'p5.js') == self.files.p5js
-        assert self.base_dir.joinpath(self.sketch_name, 'foo.py') == self.files.sketch_py
-        assert self.base_dir.joinpath(self.sketch_name, 'target_sketch.py') == self.files.target_sketch
+        assert self.get_expected_path('index.html') == self.files.index_html
+        assert self.get_expected_path('static', 'p5.js') == self.files.p5js
+        assert self.get_expected_path('foo.py') == self.files.sketch_py
+        assert self.get_expected_path('target_sketch.py') == self.files.target_sketch
 
     def test_sketch_files_holds_reference_to_lib_files(self):
         lib_files = LibFiles()
