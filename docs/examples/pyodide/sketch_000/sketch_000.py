61ff6b3850639968650dd8e9f47f73fd1e4014be
Michal Kaukič <mike@feelmath.eu>
2021-04-19 20:59:34 +0200

    Added directory doc/examples/pyodide and (compiled) sketches sketch_000 - sketch_009

---
+++ docs/examples/pyodide/sketch_000/sketch_000.py
@@ -0,0 +1,9 @@
+def setup():
+    createCanvas(200, 200)
+    background(160)
+
+def draw():
+    fill("blue")
+    background(200)
+    radius = sin(frameCount / 60) * 50 + 50
+    ellipse(100, 100, radius, radius)
