12affa94b4222e993152bae1650a71a5f2f09b29
Bernardo Fontes <bernardoxhc@gmail.com>
2021-09-20 16:37:32 -0300

    Move pyodide example to correct dir

--- docs/examples/pyodide/sketch_000/sketch_000.py
+++
@@ -1,9 +0,0 @@
-def setup():
-    createCanvas(200, 200)
-    background(160)
-
-def draw():
-    fill("blue")
-    background(200)
-    radius = sin(frameCount / 60) * 50 + 50
-    ellipse(100, 100, radius, radius)
--- docs/examples/transcrypt/sketch_012/target/python_functions.py
+++
@@ -1,5 +0,0 @@
-class PythonFunctions: pass
-
-setattr(PythonFunctions, 'map', map)
-setattr(PythonFunctions, 'filter', filter)
-setattr(PythonFunctions, 'set', set)
