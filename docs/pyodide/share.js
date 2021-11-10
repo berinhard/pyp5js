64e4feafc07a2b17d57c0a4d89140cb1ce467cb8
jvfe <jvfecav@gmail.com>
2021-11-10 09:07:52 -0300

    Copy sketch URL instead of redirecting to page

--- docs/pyodide/share.js
+++ docs/pyodide/share.js
@@ -35,3 +35,43 @@ def draw():
 
   return initialSketch;
 }
+
+// Made by user Dean Taylor in
+// https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    document.execCommand("copy");
+  } catch (err) {
+    console.error("Fallback: Oops, unable to copy URL", err);
+  }
+
+  document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(
+    function () {
+      return;
+    },
+    function (err) {
+      console.error("Async: Could not copy URL: ", err);
+    }
+  );
+}
